1727b5c1ef40d0f279ed78488443d234df012d0d
Code-Jutsu <mrimmoys@gmail.com>
2024-03-10 14:06:02 +0600

    acp

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
         protocol: "https",
         hostname: "bytebazaar-production.up.railway.app/",
       },
